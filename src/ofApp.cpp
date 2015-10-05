31c95849f16e08b25262a82b773cee5c3f6e4045
mwalczyk <mwalczyk2@gmail.com>
2015-10-05 10:48:10 -0700

    First

---
+++ src/ofApp.cpp
@@ -0,0 +1,114 @@
+#include "ofApp.h"
+
+//--------------------------------------------------------------
+void ofApp::setup()
+{
+    ofDisableArbTex();
+    ofEnableAlphaBlending();
+    
+    m_inkRenderer.setup(ofGetWidth(), ofGetHeight(), 8);
+    m_tileSaver.init(4, 0, true);
+    m_displayLines = true;
+    
+    int numCircles = (int)ofRandom(3, 6);
+    for (int i = 0; i <= numCircles; i++)
+    {
+        m_drawLocations.push_back(ofPoint(ofRandom(80, ofGetWidth()-80),
+                                          ofRandom(120, ofGetHeight()-120)));
+    }
+    m_showDrawLocations = false;
+}
+
+//--------------------------------------------------------------
+void ofApp::update()
+{
+    m_inkRenderer.update();
+    m_lineRenderer.update();
+}
+
+//--------------------------------------------------------------
+void ofApp::draw()
+{
+    m_tileSaver.begin(); //-------------------------------------------------------------- TILE RENDERER BEGINS
+    if (m_tileSaver.bGoTiling)
+    {
+        ofPushMatrix();
+        ofScale(1, -1);
+        m_inkRenderer.draw(true);
+        ofPopMatrix();
+    }
+    else
+    {
+        m_inkRenderer.draw(false);
+    }
+    ofSetColor(255, 70);
+    if (m_displayLines)
+    {
+        m_currentLine.draw();
+        for (auto &line: m_polyLines) line.draw();
+    }
+    m_tileSaver.end(); //-------------------------------------------------------------- TILE RENDERER ENDS
+    
+    
+    if (m_showDrawLocations)
+    {
+        ofSetColor(ofColor::red);
+        for (auto &p: m_drawLocations) ofCircle(p, 4);
+    }
+  
+    ofSetColor(80, 80, 80);
+    ofDrawBitmapString("Mode: " + m_inkRenderer.getDrawModeAsString(), 20, 20);
+}
+
+//--------------------------------------------------------------
+void ofApp::keyPressed(int key)
+{
+    switch (key) {
+        case 's':
+            m_tileSaver.finish("frame_" + ofToString(ofGetFrameNum()) + "_high.png", false);
+            break;
+        case 'd':
+            m_displayLines = !m_displayLines;
+            break;
+        case 't':
+            m_showDrawLocations = !m_showDrawLocations;
+            break;
+        case 'p':
+            m_inkRenderer.setDrawMode(InkRenderer::PARTICLES);
+            break;
+        case 'm':
+            m_inkRenderer.setDrawMode(InkRenderer::MANUAL);
+            break;
+        case ' ':
+            for (auto &line: m_polyLines)
+            {
+                LineFollower lf;
+                lf.setupFromPolyline(line);
+                m_lineRenderer.addFollower(lf);
+            }
+            m_lineRenderer.setup();
+            m_inkRenderer.setLineRenderer(&m_lineRenderer);
+            m_inkRenderer.setDrawMode(InkRenderer::FOLLOWERS);
+            break;
+        default:
+            break;
+    }
+}
+
+//--------------------------------------------------------------
+void ofApp::mouseDragged(int x, int y, int button)
+{
+    m_currentLine.addVertex(ofPoint(x, y));
+}
+
+//--------------------------------------------------------------
+void ofApp::mousePressed(int x, int y, int button)
+{
+    m_currentLine.clear();
+}
+
+//--------------------------------------------------------------
+void ofApp::mouseReleased(int x, int y, int button)
+{
+    m_polyLines.push_back(m_currentLine);
+}
---
+++ src/ofApp.h
@@ -0,0 +1,32 @@
+#pragma once
+
+#include "ofMain.h"
+#include "ofxTileSaver.h"
+#include "particleSystem.h"
+#include "inkRenderer.h"
+#include "lineRenderer.h"
+#define DEBUG
+
+class ofApp : public ofBaseApp
+{
+
+public:
+    
+    void setup();
+    void update();
+    void draw();
+    void keyPressed(int key);
+    void mouseDragged(int x, int y, int button);
+    void mousePressed(int x, int y, int button);
+    void mouseReleased(int x, int y, int button);
+    
+    InkRenderer             m_inkRenderer;
+    ofxTileSaver            m_tileSaver;
+    LineRenderer            m_lineRenderer;
+    vector<ofPolyline>      m_polyLines;
+    ofPolyline              m_currentLine;
+    bool                    m_displayLines;
+    bool                    m_showDrawLocations;
+    vector<ofPoint>         m_drawLocations;
+    
+};
